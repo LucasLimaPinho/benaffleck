6f6452be5ac442e5bdfcd0c84be159e03c97c844
Lucas Pinho <lucas.pinho@semantix.com.br>
2020-11-20 11:38:54 -0300

    Fix test_env

--- test_env.py
+++ test_env.py
@@ -6,8 +6,8 @@ spark_driver_host = os.environ['SPARK_DRIVER_HOST']
 spark_blockmanager_port = os.environ['SPARK_BLOCKMANAGER_PORT']
 spark_driver_bindaddress = os.environ['SPARK_DRIVER_BINDADDRESS']
 print(f"ENVIRONMENT: {environment}")
-print(f"SPARK_DRIVER_PORT: {environment}")
-print(f"SPARK_DRIVER_HOST: {environment}")
-print(f"SPARK_DRIVER_BINDADDRESS: {environment}")
-print(f"SPARK_BLOCKMANAGER_PORT: {environment}")
+print(f"SPARK_DRIVER_PORT: {spark_driver_port}")
+print(f"SPARK_DRIVER_HOST: {spark_driver_host}")
+print(f"SPARK_DRIVER_BINDADDRESS: {spark_driver_bindaddress}")
+print(f"SPARK_BLOCKMANAGER_PORT: {spark_blockmanager_port}")
 
