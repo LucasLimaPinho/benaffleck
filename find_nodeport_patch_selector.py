c3a54fce34785a5d819b49c7b831f3ffcf7adc8f
Lucas Pinho <lucas.pinho@semantix.com.br>
2020-11-20 11:12:54 -0300

    Add Spark environment variables to run spark in client-mode with driver allocated in a pod inside a argo workflow

--- find_nodeport_patch_selector.py
+++ find_nodeport_patch_selector.py
@@ -38,6 +38,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 config.load_incluster_config()
 v1 = client.CoreV1Api()
 services = []
+nodeports = []
 svc_list = v1.list_namespaced_service(namespace="argo", label_selector="app=sant,available=true")
 if (len(svc_list.items) < num):
     logger.error("There are not enough nodeports to run this workflow.")
@@ -45,11 +46,20 @@ else:
     for np in svc_list.items[:num]:
         v1.patch_namespaced_service(np.metadata.name, ns, get_body(user,np.metadata.name))
         services.append(np.metadata.name)
+        [nodeports.append(np.spec.ports[j].node_port) for j in range(len(np.spec.ports))]
 with open("/tmp/svcname1.txt","w") as f:
     f.write(services[0])
     f.close()
 with open("/tmp/svcname2.txt","w") as f:
     f.write(services[1])
     f.close()
-    print(f"Services allocated: {services}")
-    print(svc_list) 
+with open("/tmp/nodeport1.txt","w") as f:
+    f.write(nodeports[0])
+    f.close()
+with open("/tmp/nodeport2.txt","w") as f:
+    f.write(nodeports[1])
+    f.close()
+
+print(f"Services allocated: {services}")
+print(f"Nodeports allocated: {nodeports}")
+print(svc_list) 
--- test_env.py
+++ test_env.py
@@ -1,6 +1,13 @@
 import os
 
 environment = os.environ['ENVIRONMENT']
-
-print(environment)
+spark_driver_port = os.environ['SPARK_DRIVER_PORT']
+spark_driver_host = os.environ['SPARK_DRIVER_HOST']
+spark_blockmanager_port = os.environ['SPARK_BLOCKMANAGER_PORT']
+spark_driver_bindaddress = os.environ['SPARK_DRIVER_BINDADDRESS']
+print(f"ENVIRONMENT: {environment}")
+print(f"SPARK_DRIVER_PORT: {environment}")
+print(f"SPARK_DRIVER_HOST: {environment}")
+print(f"SPARK_DRIVER_BINDADDRESS: {environment}")
+print(f"SPARK_BLOCKMANAGER_PORT: {environment}")
 
